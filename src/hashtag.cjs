81c48d2db2a3397b0d1141289c2b65ef8fd7ef1d
Ilkka Huotari <ilkkah@gmail.com>
2026-07-04 19:18:30 +0300

    Update dependencies

--- src/hashtag.cjs
+++ src/hashtag.cjs
@@ -1,34 +1,34 @@
-import parser from './parser';
+const parser = require('./parser.cjs');
 
-var intRegex = /^\d+$/;
-var taskAndProjectRegex = /^([a-z][a-z0-9-]*)_(\d+)$/;
+const intRegex = /^\d+$/;
+const taskAndProjectRegex = /^([a-z][a-z0-9-]*)_(\d+)$/;
 
 function flowdockHashtag(tokens, idx) {
-  var tag = tokens[idx].content;
-  var markup = tokens[idx].markup;
-  var result = '<a ';
+  const tag = tokens[idx].content;
+  const markup = tokens[idx].markup;
+  let result = '<a ';
   if (flowdockHashtag.options && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
     if (intRegex.test(tag) && tag !== '0' && flowdockHashtag.options.pid) {
       result += 'class="task" data-tasknum="' + tag + '" data-taskpid="' + flowdockHashtag.options.pid + '"';
-    }
-    else if (intRegex.test(tag) && tag !== '0') {
+    } else if (intRegex.test(tag) && tag !== '0') {
       result += 'class="ticket" data-ticketnum="' + tag + '"';
-    }
-    else if (taskAndProjectRegex.test(tag)) {
+    } else if (taskAndProjectRegex.test(tag)) {
       result += 'class="task" data-task="' + tag + '"';
     } else {
       result += 'class="tag" href="' + flowdockHashtag.options.href_channel + encodeURIComponent('#' + tag) + '"';
     }
+  } else {
+    result += 'class="tag"';
   }
   result += '>' + markup + tag + '</a>';
 
   return result;
 }
 
-export default function (md, options) {
-  const split = "#|＃";
-  const hashtag = parser(md, 'hashtag', new RegExp(split));
-  md.core.ruler.push('hashtag', hashtag);
+module.exports = function hashtag(md, options) {
+  const split = '#|＃';
+  const hashtagParser = parser(md, 'hashtag', new RegExp(split));
+  md.core.ruler.push('hashtag', hashtagParser);
   flowdockHashtag.options = options && options.hashtags;
   md.renderer.rules.hashtag = flowdockHashtag;
 };
